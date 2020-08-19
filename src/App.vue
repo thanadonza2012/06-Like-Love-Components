f0a9f85ec7c0cc53356774454ca69a4d9f4bbddd
thanadonza2012 <68525472+thanadonza2012@users.noreply.github.com>
2020-08-19 20:38:21 +0700

    icon

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,65 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <div class="row"></div>
+    <div class="col-4"></div>
+    <img alt="Vue logo" src="./assets/1.jpg" height="300px" width="300px">
+    <img alt="Vue logo" src="./assets/2.jpg" height="300px" width="300px">
+    <img alt="Vue logo" src="./assets/3.jpg" height="300px" width="300px">
+    <img alt="Vue logo" src="./assets/4.jpg" height="300px" width="300px">
+    <img alt="Vue logo" src="./assets/5.jpg" height="300px" width="300px">
+    <img alt="Vue logo" src="./assets/6.jpg" height="300px" width="300px">
+    <img alt="Vue logo" src="./assets/7.jpg" height="300px" width="300px">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="row">
+  
+      
+      <div class="col-6">
+      </div>
+
+      <div class="col-3"> <Counter lable="ok" @cc="sum"/> </div>
+      <div class="col-3"> </div>
+      <div class="col-3"> </div>
+
+     <CsBotton/>
+        
+      
+
+    </div>
+    <div>
+      ชื่อ-นามสกุล <input type="text" v-model="name"/>
+      รหัส นศ. <input type="text " v-model="code">
+      GPA <input type="number" v-model="gpa" step="0.01"/>
+      <hr>
+      แสดง ชื่อ + รหัส นศ. gpa 
+    {{name}} {{code}} {{gpa}}
+    </div>
+    
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-
+import CsBotton from './components/Csbotton'
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+    CsBotton,
+  },
+  data :function(){
+    return{
+    total:0,
+    name:'',
+    code:'',
+    gpa:0.0,
+    }
+  },
+  methods: {
+    
+    sum(counterValue) {
+      this.total += counterValue
+    },
+  },
 }
 </script>
 
---
+++ src/components/Csbotton.vue
@@ -0,0 +1,49 @@
+<template>
+ <div>
+  <button @click="counter += 1">Add 1</button>
+  
+    <p>The button above has been clicked {{ counter }} times.</p>
+    <p v-bind:style="{fontSize: fsize + 'px'}">Hello</p>
+    <button @click="upFunction()" class ="btn btn-info"> 👍 </button>
+    <button @click="downFunction()" class ="btn btn-info"> 👎 </button>
+       <p v-bind:style="{fontSize: fsize - 'px'}"></p>
+        </div> 
+ 
+</template>
+
+<script>
+export default {
+    name: 'CsBotton',
+    data: function() {
+  return{
+    counter: 0,
+    fsize:60,
+    }
+  },
+  props:{
+    label:String,
+    
+  },
+  methods : {
+  upFunction: function(){
+    this.counter += 1
+    this.fsize += 10
+  this.$emit('cc',this.fsize)
+
+    console.log(this.fsize);
+    console.log(this.counter);
+  },
+    downFunction: function(){
+    this.counter -= 1
+    this.fsize -= 10
+
+    console.log(this.fsize);
+    console.log(this.counter);
+    },
+  },
+}
+</script>
+
+<style>
+
+</style>
